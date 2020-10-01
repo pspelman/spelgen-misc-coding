3ae44ea82621b38eaac84671994afd24c980a606
Tom Genest <32213696+gendreams@users.noreply.github.com>
2020-09-30 23:50:19 -0700

    Slightly different version
    different way to handle DOM traversal

--- dataGenerator.js
+++ dataGenerator.js
@@ -13,61 +13,62 @@ streams.users.mracus = [];
 streams.users.douglascalhoun = [];
 window.users = Object.keys(streams.users);
 
-// utility function for adding tweets to our data structures
-var addTweet = function(newTweet){
-  var username = newTweet.user;
-  streams.users[username].push(newTweet);
-  streams.home.push(newTweet);
+// utility function for adding tweets to our data structures - helper function 
+var addTweet = function(newTweet) { // self-documenting name - here comes the object 
+    var username = newTweet.user; // random element from users array - one of 4 users 
+    streams.users[username].push(newTweet); // an object is now being put into an array - the user's stream - streams.users[username] - 1/4 will go into each user 
+    streams.home.push(newTweet); // same tweet pushed into the home stream - all tweets will go there
 };
+
 
 // utility function
-var randomElement = function(array){
-  var randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+var randomElement = function(array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 };
 
-// random tweet generator
-var opening = ['How did Obama', 'Why did Trump', 'Where did your cousin', 'Why did you', 'When did your dad', 'How did Hillary', 'Why did the cat', 'When did your friend', 'How productively did your boss', 'How efficiently did your mother', 'In what manner did you', 'Where did the wizard', 'How did you', 'Where did the ninja', 'How did a seedy old man'];
-var verbs = ['download', 'interface', 'deploy', 'develop', 'build', 'invent', 'experience', 'navigate', 'aid', 'enjoy', 'engineer', 'install', 'debug', 'delegate', 'automate', 'formulate', 'systematize', 'overhaul', 'compute'];
+// random tweet generator - 5 arrays of descriptive names 
+var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var verbs = ['downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'engineered', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
-var nouns = ['cat?', 'koolaid?', 'system?', 'city?', 'worm?', 'cloud?', 'potato?', 'money?', 'way of life?', 'belief system?', 'security system?', 'bad decision?', 'future?', 'life?', 'pony?', 'mind?'];
-var tags = ['#techlife', '#burningman', '#sf', '#but only i know how', '#for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '#woke', '#namaste', '#Kobe', '#Bravo 6'];
+var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
-var randomMessage = function(){
-  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+var randomMessage = function() {
+    return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
-var generateRandomTweet = function(){
-  var tweet = {};
-  tweet.user = randomElement(users);
-  tweet.message = randomMessage();
-  tweet.created_at = new Date();
-  addTweet(tweet);
+var generateRandomTweet = function() {
+    var tweet = {}; // tweets are objects with three properties 
+    tweet.user = randomElement(users); // random element from users array (global variable)
+    tweet.message = randomMessage(); // random message - .join an array with an empty space -  of five words
+    tweet.created_at = new Date(); // timestamp 
+    addTweet(tweet); // passing an input into this function - now read code 
 };
 
-for(var i = 0; i < 10; i++){
-  generateRandomTweet();
+for (var i = 0; i < 10; i++) { // first action 
+    generateRandomTweet(); // generates 10 tweets 
 }
 
-var scheduleNextTweet = function(){
-  generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
-};
-scheduleNextTweet();
+var scheduleNextTweet = function() {
+    generateRandomTweet(); // adds new tweet to new stream 
+    setTimeout(scheduleNextTweet, Math.random() * 1500); // delays execution of provided function 
+}; // RECURSION!!! 
+scheduleNextTweet(); // controls continuous stream of tweets 
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var writeTweet = function(message){
-  if(!visitor){
-    throw new Error('set the global visitor property!');
-  }
-  if(!streams.users[visitor]) {
-    streams.users[visitor] = [];
-  }
-  var tweet = {};
-  tweet.user = visitor;
-  tweet.message = message;
-  tweet.created_at = new Date();
-  addTweet(tweet);
+var writeTweet = function(message) {
+    if (!visitor) {
+        throw new Error('set the global visitor property!');
+    }
+    if (!streams.users[visitor]) {
+        streams.users[visitor] = [];
+    }
+    var tweet = {};
+    tweet.user = visitor;
+    tweet.message = message;
+    tweet.created_at = new Date();
+    addTweet(tweet);
 };
